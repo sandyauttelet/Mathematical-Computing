6a0d6bcd99a98d42e7dc5271c6a73758ecef0174
Sandy Auttelet <107436258+sandyauttelet@users.noreply.github.com>
2024-02-13 10:14:15 -0800

    Update Plot_data_final.py

--- Plot_data_final.py
+++ Plot_data_final.py
@@ -11,14 +11,83 @@ import matplotlib.pyplot as plt
 #constants = sc.optimize.curve_fit(f_y,y_data_list_0,y_data_list_1)
     
 def f_y(t,x0,x1,x2,x3):
+    """
+    Generates function for modeled curve fitting for this specific y-data.
+    
+    Parameters
+    ----------
+    t : float
+        time of growth.
+    x0 : float
+        variable to model y-growth data.
+    x1 : float
+        variable to model y-growth data.
+    x2 : float
+        variable to model y-growth data.
+    x3 : float
+       variable to model y-growth data.
+
+    Returns
+    -------
+    f : function
+        curve found used to model this specific data.
+
+    """
     f = x0/(1+np.exp(-x1*t+x2))+x3
     return f
 
 def f_x(t,x0,x1,x2,x3,x4,x5,x6,x7,x8):
+    """
+    Generates function for modeled curve fitting for this specific x-data.
+    
+    Parameters
+    ----------
+    t : float
+        time of growth.
+    x0 : float
+        variable to model x-growth data.
+    x1 : float
+        variable to model x-growth data.
+    x2 : float
+        variable to model x-growth data.
+    x3 : float
+        variable to model x-growth data.
+    x4 : float
+        variable to model x-growth data.
+    x5 : float
+        variable to model x-growth data.
+    x6 : float
+        variable to model x-growth data.
+    x7 : float
+        variable to model x-growth data.
+    x8 : float
+        variable to model x-growth data.
+
+    Returns
+    -------
+    f : function
+        curve found used to model this specific data.
+
+    """
     f = x0*t**2 + x1*t**3 + x2*t**4 + x3*t + x4 + x5*t**5 + x6*t**6 + x7*t**7 + x8*t**8
     return f
 
 def plot_curve_fit_x(x_data_list_0,x_data_list_1):
+    """
+    Plots the fitted curve for days of growth vs. height of plant for x-plant data.
+    
+    Parameters
+    ----------
+    x_data_list_0 : list of floats
+        list of days of growth for plant data
+    x_data_list_1 : TYPE
+        list of height of growth for plant data
+
+    Returns
+    -------
+    None.
+
+    """
     tx = np.linspace(min(x_data_list_0),max(x_data_list_0),1000)
     fig1 = plt.figure(1)
     fig1.suptitle("Measured and Fit Data for x-grow", fontsize=16)
@@ -33,6 +102,21 @@ def plot_curve_fit_x(x_data_list_0,x_data_list_1):
     plt.legend(labels)
     
 def plot_curve_fit_y(y_data_list_0,y_data_list_1):
+    """
+    Plots the fitted curve for days of growth vs. height of plant for y-plant data.
+    
+    Parameters
+    ----------
+    y_data_list_0 : list of floats
+        list of days of growth for plant data
+    y_data_list_1 : TYPE
+        list of height of growth for plant data
+
+    Returns
+    -------
+    None.
+
+    """
     ty = np.linspace(min(y_data_list_0),max(y_data_list_0),1000)
     fig2= plt.figure(2)
     fig2.suptitle("Measured and Fit Data for y-grow", fontsize=16)
@@ -44,7 +128,22 @@ def plot_curve_fit_y(y_data_list_0,y_data_list_1):
     plt.legend(labels)
     
 #Plot of just x data
-def plot_x_data(x_data_list_0,x_data_list_1):    
+def plot_x_data(x_data_list_0,x_data_list_1):
+    """
+    Plots days of growth vs. height of plant for x-plant data.
+
+    Parameters
+    ----------
+    x_data_list_0 : list of floats
+        list of days of growth for plant data
+    x_data_list_1 : list of floats
+        list of height of growth for plant data
+
+    Returns
+    -------
+    None.
+
+    """    
     fig3 = plt.figure(3)
     fig3.suptitle("Growth with Fertilizer x", fontsize=16)
     plt.plot(x_data_list_0,x_data_list_1)
@@ -53,6 +152,21 @@ def plot_x_data(x_data_list_0,x_data_list_1):
 
 #Plot of just y data
 def plot_y_data(y_data_list_0,y_data_list_1):
+    """
+    Plots days of growth vs. height of plant for y-plant data.
+
+    Parameters
+    ----------
+    y_data_list_0 : list of floats
+        list of days of growth for plant data
+    y_data_list_1 : list of floats
+        list of height of growth for plant data
+
+    Returns
+    -------
+    None.
+
+    """   
     fig4 = plt.figure(4)
     fig4.suptitle("Growth with Fertilizer y", fontsize=16)
     plt.plot(y_data_list_0,y_data_list_1)
@@ -61,6 +175,25 @@ def plot_y_data(y_data_list_0,y_data_list_1):
     
 #Plot of x and y data
 def plot_x_y_data(x_data_list_0,x_data_list_1,y_data_list_0,y_data_list_1):
+    """
+    Plots days of growth vs. height of plant for both x-plant and y-plant data.
+
+    Parameters
+    ----------
+    x_data_list_0 : list of floats
+        list of days of growth for x-plant data
+    x_data_list_1 : list of floats
+        list of height of growth for x-plant data
+    y_data_list_0 : list of floats
+        list of days of growth for y-plant data
+    y_data_list_1 : list of floats
+        list of height of growth for y-plant data
+
+    Returns
+    -------
+    None.
+
+    """
     fig5 = plt.figure(5)
     fig5.suptitle("Growth with Fertilizer x and y", fontsize=16)
     plt.plot(x_data_list_0,x_data_list_1)
@@ -72,6 +205,25 @@ def plot_x_y_data(x_data_list_0,x_data_list_1,y_data_list_0,y_data_list_1):
     
 #Plot marks where 3m exists to find when we can sell
 def plot_sell_day(x_data_list_0,x_data_list_1,y_data_list_0,y_data_list_1):
+    """
+    Plots optimal sell day for both x-plant and y-plant data.
+
+    Parameters
+    ----------
+    x_data_list_0 : list of floats
+        list of days of growth for x-plant data
+    x_data_list_1 : list of floats
+        list of height of growth for x-plant data
+    y_data_list_0 : list of floats
+        list of days of growth for y-plant data
+    y_data_list_1 : list of floats
+        list of height of growth for y-plant data
+
+    Returns
+    -------
+    None.
+
+    """
     fig6 = plt.figure(6)
     fig6.suptitle("When We Can Sell", fontsize=16)
     plt.plot(x_data_list_0,x_data_list_1)
@@ -84,6 +236,29 @@ def plot_sell_day(x_data_list_0,x_data_list_1,y_data_list_0,y_data_list_1):
     
 #Plot Growth Rate
 def plot_growth_rate(x_data_list_0,x_data_list_1,y_data_list_0,y_data_list_1,x_growth_rates,y_growth_rates):
+    """
+    Plots growth rate for both x-plant and y-plant data.
+
+    Parameters
+    ----------
+    x_data_list_0 : list of floats
+        list of days of growth for x-plant data.
+    x_data_list_1 : list of floats
+        list of height of growth for x-plant data.
+    y_data_list_0 : list of floats
+        list of days of growth for y-plant data.
+    y_data_list_1 : list of floats
+        list of height of growth for y-plant data.
+    x_growth_rates : list of floats
+        growth rates for x-plant data across growth cycle.
+    y_growth_rates : list of floats
+        growth rates for x-plant data across growth cycle.
+
+    Returns
+    -------
+    None.
+
+    """
     fig7 = plt.figure(7)
     fig7.suptitle("Growth Rates", fontsize=16)
     plt.plot(x_data_list_0,x_growth_rates)
@@ -94,6 +269,21 @@ def plot_growth_rate(x_data_list_0,x_data_list_1,y_data_list_0,y_data_list_1,x_g
     plt.legend(labels)
 
 def make_barchart_x(profit_data,cycles):
+    """
+    Makes a barchart for x-plant data representing profit across each cycle of growth.
+
+    Parameters
+    ----------
+    profit_data : list of floats
+        profit per plant for each cycle.
+    cycles : list of ints
+        list of optimal cycles to determine max profit.
+
+    Returns
+    -------
+    None.
+
+    """
     fig8 = plt.figure(8)
     fig8.suptitle("60-Day Profit for Each Cycle Duration in x Fertilizer", fontsize=16)
     plt.bar(cycles,profit_data)
@@ -101,6 +291,21 @@ def make_barchart_x(profit_data,cycles):
     plt.ylabel('Profit per Plant in Ten Thousand Dollars')
     
 def make_barchart_y(profit_data,cycles):
+    """
+    Makes a barchart for y-plant data representing profit across each cycle of growth.
+
+    Parameters
+    ----------
+    profit_data : list of floats
+        profit per plant for each cycle.
+    cycles : list of ints
+        list of optimal cycles to determine max profit.
+
+    Returns
+    -------
+    None.
+
+    """
     fig9 = plt.figure(9)
     fig9.suptitle("60-Day Profit for Each Cycle Duration in y Fertilizer", fontsize=16)
     plt.bar(cycles,profit_data)
@@ -108,6 +313,25 @@ def make_barchart_y(profit_data,cycles):
     plt.ylabel('Profit per Plant in Ten Thousand Dollars')
     
 def plot_cycle_growth_x(x_list_optimal_0,x_list_optimal_1,x_optimal_cycles,x_optimal_average_growth):
+    """
+    Plots growth cycle for x-plant data marking averages and optimal cycles.
+
+    Parameters
+    ----------
+    x_list_optimal_0 : list of floats
+        list of days of optimal growth for x-plant data.
+    x_list_optimal_1 : list of floats
+        list of height of optimal growth for x-plant data.
+    x_optimal_cycles : list of floats
+        list of optimal cycles for x-plant data.
+    x_optimal_average_growth : float
+        average growth rate for optimal cut time.
+
+    Returns
+    -------
+    None.
+
+    """
     fig10 = plt.figure(10)
     fig10.suptitle(f'{x_optimal_cycles} Consistent {len(x_list_optimal_0)}-Day Growth Cycle(s) in x Fertilizer', fontsize=16)
     for i in range(int(x_optimal_cycles)):
@@ -120,6 +344,25 @@ def plot_cycle_growth_x(x_list_optimal_0,x_list_optimal_1,x_optimal_cycles,x_opt
     plt.ylabel('Height (m)')
     
 def plot_cycle_growth_y(y_list_optimal_0,y_list_optimal_1,y_optimal_cycles,y_optimal_average_growth):
+    """
+    Plots growth cycle for y-plant data marking averages and optimal cycles.
+
+    Parameters
+    ----------
+    y_list_optimal_0 : list of floats
+        list of days of optimal growth for y-plant data.
+    y_list_optimal_1 : list of floats
+        list of height of optimal growth for y-plant data.
+    y_optimal_cycles : list of floats
+        list of optimal cycles for y-plant data.
+    y_optimal_average_growth : float
+        average growth rate for optimal cut time.
+
+    Returns
+    -------
+    None.
+
+    """
     fig11 = plt.figure(11)
     fig11.suptitle(f'{y_optimal_cycles} Consistent {len(y_list_optimal_0)}-Day Growth Cycle(s) in y Fertilizer', fontsize=16)
     for i in range(int(y_optimal_cycles)):
@@ -132,6 +375,21 @@ def plot_cycle_growth_y(y_list_optimal_0,y_list_optimal_1,y_optimal_cycles,y_opt
     plt.ylabel('Height (m)')
     
 def plot_risk_x(x_risks,x_list_0):
+    """
+    Plots calcuated risk as it varies across the days of growth.
+
+    Parameters
+    ----------
+    x_risks : list of floats
+        financial risk associated with keeping plants growing at each day.
+    x_list_0 : list of floats
+        days of growth for x-plant data.
+
+    Returns
+    -------
+    None.
+
+    """
     fig12 = plt.figure(12)
     fig12.suptitle('Risk per Plant of x fertilizer')
     risks = []
@@ -142,6 +400,21 @@ def plot_risk_x(x_risks,x_list_0):
     plt.ylabel('Risk per Plant per One-Hundred Thousand Dollars')
     
 def plot_risk_y(y_risks,y_list_0):
+    """
+    Plots calcuated risk as it varies across the days of growth.
+
+    Parameters
+    ----------
+    y_risks : list of floats
+        financial risk associated with keeping plants growing at each day.
+    y_list_0 : list of floats
+        days of growth for y-plant data.
+
+    Returns
+    -------
+    None.
+
+    """
     fig13 = plt.figure(13)
     fig13.suptitle('Risk per Plant of x fertilizer')
     risks = []
@@ -152,6 +425,23 @@ def plot_risk_y(y_risks,y_list_0):
     plt.ylabel('Risk per Plant per One-Hundred Thousand Dollars')
     
 def plot_risks_x(x_risks,x_optimal_cycles,x_list_0):
+    """
+    Plots all risks associated with growth x-plants.
+
+    Parameters
+    ----------
+    x_risks : list of floats
+        financial risk associated with keeping plants growing at each day.
+    x_optimal_cycles : list of floats
+        list of optimal cycles for x-plant data.
+    x_list_0 : list of floats
+        days of growth for x-plant data.
+
+    Returns
+    -------
+    None.
+
+    """
     fig14 = plt.figure(14)
     #fig14.suptitle('Risk per Plant of x fertilizer')
     risks = []
@@ -162,6 +452,23 @@ def plot_risks_x(x_risks,x_optimal_cycles,x_list_0):
     plt.ylabel('Risk per Plant in Dollars')
     
 def plot_risks_y(y_risks,y_optimal_cycles,y_list_0):
+    """
+    Plots all risks associated with growth y-plants.
+
+    Parameters
+    ----------
+    y_risks : list of floats
+        financial risk associated with keeping plants growing at each day.
+    y_optimal_cycles : list of floats
+        list of optimal cycles for y-plant data.
+    y_list_0 : list of floats
+        days of growth for y-plant data.
+
+    Returns
+    -------
+    None.
+
+    """
     fig15 = plt.figure(15)
     #fig15.suptitle('Risk per Plant of y fertilizer')
     risks = []
@@ -172,6 +479,29 @@ def plot_risks_y(y_risks,y_optimal_cycles,y_list_0):
     plt.ylabel('Risk per Plant in Dollars')
     
 def plot_risks_x_and_y(x_risks,x_optimal_cycles,x_list_0,y_risks,y_optimal_cycles,y_list_0):
+    """
+    Plots all risks associated with growth of both x-plant and y-plant data.
+
+    Parameters
+    ----------
+    x_risks : list of floats
+        financial risk associated with keeping plants growing at each day.
+    x_optimal_cycles : list of floats
+        list of optimal cycles for x-plant data.
+    x_list_0 : list of floats
+        days of growth for x-plant data.
+    y_risks : list of floats
+        financial risk associated with keeping plants growing at each day.
+    y_optimal_cycles : list of floats
+        list of optimal cycles for y-plant data.
+    y_list_0 : list of floats
+        days of growth for y-plant data.
+
+    Returns
+    -------
+    None.
+
+    """
     fig16 = plt.figure(16)
     #fig16.suptitle('Risk per Plant of x and y fertilizer')
     x_risks_changed = []
